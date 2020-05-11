e9494712e14ec5e6affc600c2092933584faeeed
Olexyy <olexyy.mails@gmail.com>
2020-05-11 21:32:45 +0300

    refactor

--- server/app.js
+++ server/app.js
@@ -1,157 +1,121 @@
-const express    = require('express');
-const path       = require('path');
-const port       = process.env.PORT || 3000;
-const app        = express();
-const fs         = require('fs');
-var http         = require('http').createServer(app);
-const storage    = path.join(__dirname, "..", 'storage');
+class App {
 
-/**
- * WebSocket Configuration
- */
-var io = require('socket.io')(http, {});
-
-/**
-* Pocker backend implementation.
-*/
-const lpNs = '/lp';
-const lp = io.of(lpNs);
-const lpApp = {
-    messages: [],
-    announcements: [],
-    topics: {
-        culture: 'culture',
-        medicine: 'medicine',
-        business: 'business',
+    constructor(io) {
+        this.messages = [];
+        this.announcements = [];
+        this.topics = {
+            culture: 'culture',
+            medicine: 'medicine',
+            business: 'business',
+        };
+        this.namespace = '/lp';
+        this.io = io;
+        this.sockets = this.io.of(this.namespace);
     }
-};
-lp.on('connect', function(socket) {
-    console.log('socket connected');
-    socket.on('error', e => {
-        console.log('socket error');
-    });
-    socket.on('disconnect', () => {
-        console.log('socket disconnect');
-    });
-    socket.on('liveness', () => {
-        console.log('socket is live');
-    });
-    socket.on('status', () => {
-        const userRooms = Object.keys(lp.adapter.sids[socket.id]);
-        userRooms.shift();
-        lp.to(socket.id).emit('status', {
-            topics: lpApp.topics,
-            rooms: userRooms,
-        });
-        console.log('socket status');
-    });
-    // May be array.
-    socket.on('join', rooms => {
-        socket.join(rooms, function(e) {
-            console.log('joining: ' + rooms);
-            const userRooms = Object.keys(lp.adapter.sids[socket.id]);
-            userRooms.shift();
-            let announcements = [];
-            // Todo implenet skip / take functions.
-            lpApp.announcements.forEach(item => {
-                let include = userRooms.filter(value => item.topics.includes(value));
-                if (include.length) {
-                    announcements.push(item);
-                }
+
+    bind() {
+        this.sockets.on('connect', socket => {
+            console.log('socket connected');
+            socket.on('error', e => {
+                console.log('socket error');
             });
-            console.log(announcements);
-            lp.to(socket.id).emit('status', {
-                topics: lpApp.topics,
-                rooms: userRooms,
-                announcements: announcements
+            socket.on('disconnect', () => {
+                console.log('socket disconnect');
             });
-            console.log('socket join complete');
+            socket.on('liveness', () => {
+                console.log('socket is live');
+            });
+            socket.on('status', () => {
+                this.sockets.to(socket.id).emit('status', {
+                    topics: this.topics,
+                    rooms: this.getRooms(socket.id),
+                });
+                console.log('socket status');
+            });
+            // May be array.
+            socket.on('join', rooms => {
+                socket.join(rooms, e => {
+                    console.log('joining: ' + rooms);
+                    this.sockets.to(socket.id).emit('status', {
+                        topics: this.topics,
+                        rooms: this.getRooms(socket.id),
+                        announcements: this.getAnnouncements(socket.id),
+                    });
+                    console.log('socket join complete');
+                });
+            });
+            socket.on('leave',  room => {
+                socket.leave(room);
+                this.sockets.to(socket.id).emit('status', {
+                    topics: this.topics,
+                    rooms: this.getRooms(socket.id),
+                    announcements: this.getAnnouncements(socket.id)
+                });
+                console.log('socket leave');
+            });
+            socket.on('announcement', (mes) => {
+                const message = this.parseMessage(mes, socket.id);
+                const delivery = this.getDelivery(message, socket.id);
+                this.addAnnouncement(message);
+                delivery.emit('announcement', message);
+            });
+            socket.on('message', (mes) => {
+                const message = this.parseMessage(mes, socket.id);
+                const delivery = this.getDelivery(message, socket.id);
+                this.addMessage(message);
+                delivery.emit('message', message);
+            });
         });
-    });
-    socket.on('leave',  room => {
-        socket.leave(room);
-        const userRooms = Object.keys(lp.adapter.sids[socket.id]);
+
+        return this;
+    }
+    
+    getRooms(id) {
+        let userRooms = Object.keys(this.sockets.adapter.sids[id]);
         userRooms.shift();
+        return userRooms;
+    }
+
+    getDelivery(mes, id) {
+        const userRooms = this.getRooms(id);
+        let delivery = this.sockets.to(id);
+        this.sockets.to(id);
+        userRooms.forEach(room => {
+            delivery = delivery.to(room);
+            mes.topics[room] = room;
+        });
+        return delivery;
+    }
+
+    parseMessage(mes, id) {
+        return {
+            text: mes.text,
+            name: mes.name,
+            author: id,
+            time: new Date().getTime(),
+            topics: mes.topics || [],
+        };
+    }
+
+    addAnnouncement(announcement) {
+        this.announcements.unshift(announcement);
+    }
+
+    addMessage(message) {
+        this.messages.unshift(message);
+    }
+
+    getAnnouncements(id) {
+        const userRooms = this.getRooms(id);
         let announcements = [];
-        // Todo implenet skip / take functions.
-        lpApp.announcements.forEach(item => {
+        this.announcements.forEach(item => {
             let include = userRooms.filter(value => item.topics.includes(value));
             if (include.length) {
                 announcements.push(item);
             }
         });
-        console.log(announcements);
-        lp.to(socket.id).emit('status', {
-            topics: lpApp.topics,
-            rooms: userRooms,
-            announcements: announcements
-        });
-        console.log('socket leave');
-    });
-    socket.on('announcement', (mes) => {
-        message = {
-            text: mes.text,
-            name: mes.name,
-            author: socket.id,
-            time: new Date().getTime(),
-            topics: mes.topics || [],
-        };
-        const userRooms = Object.keys(lp.adapter.sids[socket.id]);
-        userRooms.shift();
-        console.log(userRooms);
-        let delivery = lp.to(socket.id);
-        userRooms.forEach(room => {
-            delivery = delivery.to(room);
-            message.topics[room] = room;
-        });
-        lpApp.announcements.unshift(message);
-        delivery.emit('announcement', message);
-    });
-    socket.on('message', (mes) => {
-        message = {
-            text: mes.text,
-            name: mes.name,
-            author: socket.id,
-            time: new Date().getTime(),
-            topics: mes.topics || [],
-        };
-        const userRooms = Object.keys(lp.adapter.sids[socket.id]);
-        userRooms.shift();
-        console.log(userRooms);
-        let delivery = lp.to(socket.id);
-        userRooms.forEach(room => {
-            delivery = delivery.to(room);
-            message.topics[room] = room;
-        });
-        lpApp.messages.unshift(message);
-        delivery.emit('message', message);
-    });
-});
-/**
- *   Storage
- */
-app.use(express.static(path.join(storage)));
-
-app.get('/', function (req, res) {
-    res.sendFile(path.join(storage, `index.html`));
-});
-
-app.get('/:file', function (req, res) {
-    const fileName = req.params.file;
-    const filePath = path.join(storage, `${fileName}.html`);
-    if (fs.existsSync(filePath)) {
-      res.sendFile(filePath);
+        return announcements;
     }
-    else {
-      res.status(404).end('Not found');
-    }
-});
+}
 
-http.listen(port, () => console.log(`\x1b[40m`,`\x1b[32m`,
-`
-    [+] Server         : http://0.0.0.0:${port}
-    [+] Storage Path   : ${storage}
-
-    [~] Running Server...
-
-`,`\x1b[0m`));
+module.exports = App;
---
+++ server/server.js
@@ -0,0 +1,45 @@
+const App        = require('./App.js');
+const Express    = require('express');
+const path       = require('path');
+const port       = process.env.PORT || 3000;
+const express    = Express();
+const fs         = require('fs');
+var http         = require('http').createServer(express);
+const storage    = path.join(__dirname, "..", 'storage');
+
+/**
+ * WebSocket Configuration
+ */
+var io = require('socket.io')(http, {});
+/**
+* App backend socket.io implementation.
+*/
+new App(io).bind();
+/**
+ *   Storage
+ */
+express.use(Express.static(path.join(storage)));
+
+express.get('/', function (req, res) {
+    res.sendFile(path.join(storage, `index.html`));
+});
+
+express.get('/:file', function (req, res) {
+    const fileName = req.params.file;
+    const filePath = path.join(storage, `${fileName}.html`);
+    if (fs.existsSync(filePath)) {
+      res.sendFile(filePath);
+    }
+    else {
+      res.status(404).end('Not found');
+    }
+});
+
+http.listen(port, () => console.log(`\x1b[40m`,`\x1b[32m`,
+`
+    [+] Server         : http://0.0.0.0:${port}
+    [+] Storage Path   : ${storage}
+
+    [~] Running Server...
+
+`,`\x1b[0m`));
